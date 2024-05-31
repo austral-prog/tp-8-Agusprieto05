25b5df32b2704cbe3a9ed6abcfaf43fd4ab184c2
Agusprieto05 <162721778+Agusprieto05@users.noreply.github.com>
2024-05-30 21:36:08 -0300

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,19 +1,21 @@
 def get_coordinate(record):
-    (a, b) = record
-    return b
+    treasure, place = record
+    return place
+
+print(get_coordinate(("fdfdsafdsafd", "2A")))
+
 
 def convert_coordinate(coordinate):
-    a, b = coordinate
-    return (a, b)
+    place = coordinate
+    return tuple(place)
 
 print(convert_coordinate("2A"))
 
 def create_record(azara_record, rui_record):
-    (a, b) = azara_record
-    (c, d, e) = rui_record
-    if convert_coordinate(b) == d:
-        new_record = (azara_record + rui_record)
-        return new_record
+    what, position = azara_record
+    place, point, colour = rui_record
+    position = tuple(position)
+    if position == point:
+        return what,position,place,point,colour
     else:
-        return "not a match"
-
+        return "No coincidence"
